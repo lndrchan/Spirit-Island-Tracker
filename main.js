e786b8e834515f3a62c0ed5f0f44578557960969
lndrchan <untitledaep@gmail.com>
2026-01-26 04:21:37 +0000

    Jan 26, 2026, 4:21 AM

--- main.js
+++ main.js
@@ -262,7 +262,7 @@ function advancePhase(count) {
 
     // If phase list empty, populate it before anything else
     if ($('.list-group-item', phaseList).length <= 0) {
-
+        
     }
 
     for (let i = 0; i < count; i++) {
